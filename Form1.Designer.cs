1086ebc2eb086a48f1fc891620d6ba54b10f3141
pn.thinh <pn.thinh1@gmail.com>
2019-06-23 10:07:29 +0700

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace WF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.open = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // open
+            // 
+            this.open.Location = new System.Drawing.Point(88, 111);
+            this.open.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.open.Name = "open";
+            this.open.Size = new System.Drawing.Size(100, 57);
+            this.open.TabIndex = 0;
+            this.open.Text = "Open";
+            this.open.UseVisualStyleBackColor = true;
+            this.open.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(244, 111);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(105, 57);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "OK";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(88, 208);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(261, 24);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(436, 346);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.open);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "PNT";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button open;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using Microsoft.WindowsAPICodePack.Shell;
+using Microsoft.WindowsAPICodePack.Shell.PropertySystem;
+
+namespace WF
+{
+    public partial class Form1 : Form
+    {
+        string folderpath = @"C:\Image";
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.Items.Add(@"https://myteashirts.com/");
+            comboBox1.Items.Add(@"https://pdtshirt.com/");
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog();
+            dialog.InitialDirectory = "C:\\";
+            dialog.IsFolderPicker = true;
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                //MessageBox.Show("You selected: " + dialog.FileName);
+                folderpath = dialog.FileName;
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            doIt();
+        }
+        void doIt()
+        {
+            DirectoryInfo d = new DirectoryInfo(folderpath);
+
+            foreach (var file in d.GetFiles("*.jpg"))
+            {
+                string filePath = file.FullName;
+                string name = Path.GetFileNameWithoutExtension(filePath);
+                var img = ShellFile.FromFilePath(filePath);
+                try
+                {
+                    ShellPropertyWriter propertyWriter = img.Properties.GetPropertyWriter();
+                    propertyWriter.WriteProperty(SystemProperties.System.Title, name);
+                    propertyWriter.WriteProperty(SystemProperties.System.Subject, name);
+                    propertyWriter.WriteProperty(SystemProperties.System.Rating, 99);
+                    propertyWriter.WriteProperty(SystemProperties.System.Comment, "I like " + name);
+                    propertyWriter.WriteProperty(SystemProperties.System.Title, name);
+                    propertyWriter.WriteProperty(SystemProperties.System.Author, new string[] { comboBox1.Text });
+                    propertyWriter.WriteProperty(SystemProperties.System.Copyright, comboBox1.Text);
+                    propertyWriter.Close();
+                    // Read and Write:
+                    /*
+                    img.Properties.System.Title.Value = file.Name;
+                    img.Properties.System.Subject.Value = file.Name;
+                    img.Properties.System.Rating.Value = 99; // 5 stars
+                    img.Properties.System.Comment.Value = "I like " + file.Name;
+                    img.Properties.System.Author.Value = new string[] { comboBox1.Text };
+                    img.Properties.System.Copyright.Value = comboBox1.Text; */
+                }
+                catch(Exception e)
+                {
+                    //MessageBox.Show("Error !!!");
+                    continue;
+                }                
+            }
+        }
+    }
+}
+
+/*
+ TAI LIEU THAM KHAO: 
+ *https://stackoverflow.com/questions/11861151/find-all-files-in-a-folder
+string filepath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+DirectoryInfo d = new DirectoryInfo(filepath);
+
+foreach (var file in d.GetFiles("*.txt"))
+{
+      Directory.Move(file.FullName, filepath + "\\TextFiles\\" + file.Name);
+}
+
+ *https://stackoverflow.com/questions/5337683/how-to-set-extended-file-properties# 
+ using Microsoft.WindowsAPICodePack.Shell;
+using Microsoft.WindowsAPICodePack.Shell.PropertySystem;
+
+string filePath = @"C:\temp\example.docx";
+var file = ShellFile.FromFilePath(filePath);
+
+// Read and Write:
+
+string[] oldAuthors = file.Properties.System.Author.Value;
+string oldTitle = file.Properties.System.Title.Value;
+
+file.Properties.System.Author.Value = new string[] { "Author #1", "Author #2" };
+file.Properties.System.Title.Value = "Example Title";
+
+// Alternate way to Write:
+
+ShellPropertyWriter propertyWriter =  file.Properties.GetPropertyWriter();
+propertyWriter.WriteProperty(SystemProperties.System.Author, new string[] { "Author" });
+propertyWriter.Close();
+*/
